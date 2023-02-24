dbecdce3a31c26d4b3e4f4591e5b3a2f788bb732
Simgenaz Yalçınkaya <simge.naz.y@outlook.com>
2023-02-24 23:20:07 +0300

    React-Basic-Product-Category-App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
+function App() {
+  return (
+    <div >
+      <Navi></Navi>
+      <CategoryList></CategoryList>
+      <ProductList/>
+    </div>
+  );
+}
+
+export default App;
